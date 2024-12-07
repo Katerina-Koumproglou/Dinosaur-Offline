f55824c1675a9f83c2229678574050ce2850e220
Katerina Koumproglou <kat.koumproglou@gmail.com>
2024-12-07 16:20:26 +0200

    Sound effects added

--- dino.js
+++ dino.js
@@ -1,7 +1,7 @@
 //Board
 let board;
 let boardWidth = 750;
-let boardHeight = 250;
+let boardHeight = 300;
 let context;
 
 //Dino
@@ -16,6 +16,7 @@ let dino = {
   y: dinoY,
   width: dinoWidth,
   height: dinoHeight,
+  img: null,
 };
 
 //Ducking dino
@@ -23,13 +24,6 @@ let dinoDuckWidth = 118;
 let dinoDuckHeight = 60;
 let dinoDuckY = boardHeight - dinoDuckHeight;
 let dinoDuckImg;
-
-let dinoDuck = {
-  x: dinoX,
-  y: dinoDuckY,
-  width: dinoDuckWidth,
-  height: dinoDuckHeight,
-};
 
 //Cactus
 let cactusArray = [];
@@ -56,6 +50,19 @@ let gameOverImg;
 let scoreSpeed = 0.2;
 let score = 0;
 
+//Replay
+let replayImg;
+let replayButton = {
+  x: 0,
+  y: 0,
+  width: 0,
+  height: 0,
+};
+
+//Sounds
+let jumpSound;
+let deathSound;
+
 window.onload = function () {
   board = document.getElementById("board");
   board.height = boardHeight;
@@ -67,11 +74,10 @@ window.onload = function () {
   //   context.fillStyle = "green";
   //   context.fillRect(dino.x, dino.y, dino.width, dino.height);
 
+  //Loading images
   dinoImg = new Image();
   dinoImg.src = "./images/dino.png";
-  dinoImg.onload = function () {
-    context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
-  };
+  dino.img = dinoImg;
 
   dinoDuckImg = new Image();
   dinoDuckImg.src = "./images/dino-duck1.png";
@@ -85,21 +91,47 @@ window.onload = function () {
   cactus3Img = new Image();
   cactus3Img.src = "./images/cactus3.png";
 
+  dinoDeadImg = new Image();
+  dinoDeadImg.src = "./images/dino-dead.png";
+
   gameOverImg = new Image();
   gameOverImg.src = "./images/game-over.png";
+
+  replayImg = new Image();
+  replayImg.src = "./images/replay.png";
+
+  //Loading sounds
+  jumpSound = new Audio("./sounds/jump.mp3");
+  deathSound = new Audio("./sounds/death.mp3");
 
   requestAnimationFrame(update);
   setInterval(placeCactus, 1000); //1000 milliseconds = 1 second
   document.addEventListener("keydown", moveDino);
   document.addEventListener("keyup", stopDucking);
+  board.addEventListener("click", replayClick);
+  board.addEventListener("mousemove", replayClick);
 };
 
 function update() {
   if (gameOver) {
+    //Game Over image placement
     context.drawImage(
       gameOverImg,
       boardWidth / 2 - gameOverImg.width / 2,
-      boardHeight / 2 - gameOverImg.height / 2
+      boardHeight / 2 - gameOverImg.height / 2 - 75
+    );
+
+    //Replay image placement
+    replayButton.x = boardWidth / 2 - replayImg.width / 2;
+    replayButton.y = boardHeight / 2 - replayImg.height / 2;
+    replayButton.width = replayImg.width;
+    replayButton.height = replayImg.height;
+    context.drawImage(
+      replayImg,
+      replayButton.x,
+      replayButton.y,
+      replayButton.width,
+      replayButton.height
     );
     return;
   }
@@ -110,8 +142,8 @@ function update() {
 
   //Dino
   velocityY += gravity;
-  dino.y = Math.min(dino.y + velocityY, dinoY);
-  context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+  dino.y = Math.min(dino.y + velocityY, boardHeight - dino.height);
+  context.drawImage(dino.img, dino.x, dino.y, dino.width, dino.height);
 
   //Cactus
   for (let i = 0; i < cactusArray.length; i++) {
@@ -127,9 +159,16 @@ function update() {
 
     if (detectCollision(dino, cactus)) {
       gameOver = true;
-      dinoImg.src = "./images/dino-dead.png";
-      dinoImg.onload = function () {
-        context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
+      isDucking = false;
+      dino.width = dinoWidth;
+      dino.height = dinoHeight;
+      dino.y = dinoY;
+      dino.img = dinoDeadImg;
+      deathSound.play();
+      dino.img.src = "./images/dino-dead.png";
+      context.clearRect(0, 0, board.width, board.height);
+      dino.img.onload = function () {
+        context.drawImage(dino.img, dino.x, dino.y, dino.width, dino.height);
       };
     }
   }
@@ -149,19 +188,15 @@ function moveDino(e) {
 
   if ((e.code == "Space" || e.code == "ArrowUp") && dino.y == dinoY) {
     velocityY = -10;
+    jumpSound.currentTime = 0;
+    jumpSound.play();
   } else if (e.code == "ArrowDown" && dino.y == dinoY) {
     if (!isDucking) {
       isDucking = true;
-      dinoDuckImg.src = "./images/dino-duck1.png";
-      dinoDuckImg.onload = function () {
-        context.drawImage(
-          dinoDuckImg,
-          dinoDuck.x,
-          dinoDuck.y,
-          dinoDuck.width,
-          dinoDuck.height
-        );
-      };
+      dino.img = dinoDuckImg;
+      dino.width = dinoDuckWidth;
+      dino.height = dinoDuckHeight;
+      dino.y = dinoDuckY;
     }
   }
 }
@@ -169,10 +204,10 @@ function moveDino(e) {
 function stopDucking(e) {
   if (e.code == "ArrowDown" && isDucking) {
     isDucking = false;
-    dinoImg.src = "./images/dino.png";
-    dinoImg.onload = function () {
-      context.drawImage(dinoImg, dino.x, dino.y, dino.width, dino.height);
-    };
+    dino.img = dinoImg;
+    dino.width = dinoWidth;
+    dino.height = dinoHeight;
+    dino.y = dinoY;
   }
 }
 
@@ -211,6 +246,42 @@ function placeCactus() {
   if (cactusArray.length > 5) {
     cactusArray.shift();
   }
+}
+
+function replayClick(e) {
+  if (!gameOver) return;
+  const mouseX = e.offsetX;
+  const mouseY = e.offsetY;
+
+  if (
+    mouseX >= replayButton.x &&
+    mouseX <= replayButton.x + replayButton.width &&
+    mouseY >= replayButton.y &&
+    mouseY <= replayButton.y + replayButton.height
+  ) {
+    board.style.cursor = "pointer";
+
+    if (e.type === "click") {
+      restartGame();
+    }
+  } else {
+    board.style.cursor = "default";
+  }
+}
+
+function restartGame() {
+  gameOver = false;
+  dino.img = dinoImg;
+  dino.x = dinoX;
+  dino.y = dinoY;
+  dino.width = dinoWidth;
+  dino.height = dinoHeight;
+
+  velocityX = -8;
+  velocityY = 0;
+  cactusArray = [];
+  score = 0;
+  requestAnimationFrame(update);
 }
 
 function detectCollision(a, b) {
